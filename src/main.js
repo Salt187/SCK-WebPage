54e77721e4ddd7a0c7c06015c376f2cd160c07e7
sea-salt-mint-cake <1873768185@qq.com>
2025-11-13 22:33:03 +0800

    修正了element-plus的中文显示问题

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import {createPersistedState} from "pinia-persistedstate-plugin";
+import zhCn from 'element-plus/es/locale/lang/zh-cn' // 引入element-plus的中文语言包
 
 //导入pinia
 import {createPinia} from 'pinia'
@@ -18,5 +19,5 @@ import router from '@/router'
 const app = createApp(App);
 app.use(pinia);
 app.use(router);
-app.use(ElementPlus);
+app.use(ElementPlus, { locale: zhCn }); // 配置ElementPlus并全局启用中文
 app.mount('#app')
